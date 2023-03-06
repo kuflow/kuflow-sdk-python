5de94a6cc736a90356d88dbec23f1b1b6be5c86d
Zebensui Méndez Corujo <zmencor@kuflow.com>
2023-03-06 08:51:47 +0000

    Release 0.4.1

--- kuflow-temporal-activity-kuflow/kuflow_temporal_activity_kuflow/__init__.py
+++ kuflow-temporal-activity-kuflow/kuflow_temporal_activity_kuflow/__init__.py
@@ -27,4 +27,4 @@ from .kuflow_async_activities import KuFlowAsyncActivities
 from .kuflow_sync_activities import KuFlowSyncActivities
 
 __all__ = ["KuFlowAsyncActivities", "KuFlowSyncActivities"]
-__version__ = "0.4.1.dev0"
+__version__ = "0.4.1"
--- kuflow-temporal-common/kuflow_temporal_common/__init__.py
+++ kuflow-temporal-common/kuflow_temporal_common/__init__.py
@@ -24,4 +24,4 @@
 #
 
 # __all__ = ["KuFlowPayloadConverter"]
-__version__ = "0.4.1.dev0"
+__version__ = "0.4.1"
