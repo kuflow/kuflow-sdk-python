8dde8899ddefd8dbcd6f2c2b1ac217a6f9919e1f
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2023-06-19 18:57:27 +0100

    Adapt _test_worker.py to the new simplified connection

--- kuflow-temporal-activity-kuflow/test/_test_worker.py
+++ kuflow-temporal-activity-kuflow/test/_test_worker.py
@@ -1,17 +1,47 @@
+# coding=utf-8
+#
+# MIT License
+#
+# Copyright (c) 2022 KuFlow
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 import asyncio
 import logging
-import dataclasses
 import yaml
 
-from temporalio.client import Client, TLSConfig
-from temporalio.worker import Worker
-from temporalio.converter import DataConverter
+from temporalio.client import TLSConfig
 
-from kuflow_temporal_activity_kuflow.converter import KuFlowPayloadConverter
-from kuflow_temporal_common.authentication import KuFlowAuthorizationTokenProvider
 from kuflow_rest import KuFlowRestClient
+
 from kuflow_temporal_activity_kuflow import KuFlowAsyncActivities
 from kuflow_temporal_activity_kuflow import KuFlowSyncActivities
+from kuflow_temporal_common.connection import (
+    KuFlowTemporalConnection,
+    KuFlowConfig,
+    TemporalConfig,
+    TemporalClientConfig,
+    TemporalWorkerConfig,
+)
+
+
 from _test_workflow import GreetingWorkflow
 
 # Load configuration
@@ -40,52 +70,39 @@ async def main():
     # Uncomment the line below to see logging
     logging.basicConfig(level=logging.INFO)
 
-    kuflow_client = KuFlowRestClient(
+    kuflow_rest_client = KuFlowRestClient(
         client_id=client_id,
         client_secret=client_secret,
         endpoint=endpoint,
-        allow_insecure_connection=True,
     )
 
-    # Create an KuFlow token provider
-    kuFlow_authorization_token_provider = KuFlowAuthorizationTokenProvider(kuflow_client=kuflow_client)
+    kuflow_sync_activities = KuFlowSyncActivities(kuflow_rest_client)
+    kuflow_async_activities = KuFlowAsyncActivities(kuflow_rest_client)
 
-    client = await Client.connect(
-        temporal_host,
-        namespace=temporal_namespace,
-        tls=TLSConfig(
-            server_root_ca_cert=server_root_ca_cert,
-            client_cert=client_cert,
-            client_private_key=client_key,
+    activities = kuflow_sync_activities.activities + kuflow_async_activities.activities
+
+    kuflow_temporal_connection = KuFlowTemporalConnection(
+        kuflow=KuFlowConfig(rest_client=kuflow_rest_client),
+        temporal=TemporalConfig(
+            client=TemporalClientConfig(
+                target_host=temporal_host,
+                namespace=temporal_namespace,
+                tls=TLSConfig(
+                    server_root_ca_cert=server_root_ca_cert,
+                    client_cert=client_cert,
+                    client_private_key=client_key,
+                ),
+            ),
+            worker=TemporalWorkerConfig(
+                task_queue=temporal_queue,
+                workflows=[GreetingWorkflow],
+                activities=activities,
+                debug_mode=True,
+            ),
         ),
-        rpc_metadata=kuFlow_authorization_token_provider.initialize_rpc_auth_metadata(),
-        data_converter=dataclasses.replace(
-            DataConverter.default,
-            payload_converter_class=KuFlowPayloadConverter,
-        ),
     )
 
-    kuFlow_authorization_token_provider.start_auto_refresh(client)
-
-    # Run a worker for the workflow
-    kuflow_sync_activities = KuFlowSyncActivities(kuflow_client)
-    kuflow_async_activities = KuFlowAsyncActivities(kuflow_client)
-
-    worker = Worker(
-        client,
-        debug_mode=True,  # Debug mode
-        task_queue=temporal_queue,
-        workflows=[GreetingWorkflow],
-        activities=kuflow_sync_activities.activities + kuflow_async_activities.activities,
-        # In order to debug in vscode, use UnsandboxedWorkflowRunner "from temporalio.worker":
-        #   workflow_runner=UnsandboxedWorkflowRunner()
-        # and (optionally if you want set a breakpoint inside Temporal code):
-        #   1) Add temporalio package to the workspace to breakpoint inside,
-        #   2) Launch with justMyCode": false in launch configuration
-        # More info: https://github.com/temporalio/sdk-python/issues/238
-    )
-
-    await worker.run()
+    await kuflow_temporal_connection.run_worker()
 
 
 if __name__ == "__main__":
