a5a7257163b582306857f32c3dcb5d10525d2fae
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2023-09-19 14:31:38 +0100

    Release 1.1.1

--- kuflow-rest/kuflow_rest/__init__.py
+++ kuflow-rest/kuflow_rest/__init__.py
@@ -27,4 +27,4 @@ from ._generated._serialization import Deserializer, Model, Serializer
 from ._kuflow_rest_client import KuFlowRestClient
 
 __all__ = ["Deserializer", "KuFlowRestClient", "Model", "Serializer"]
-__version__ = "1.1.1.dev0"
+__version__ = "1.1.1"
--- kuflow-temporal-activity-robotframework/kuflow_temporal_activity_robotframework/__init__.py
+++ kuflow-temporal-activity-robotframework/kuflow_temporal_activity_robotframework/__init__.py
@@ -27,4 +27,4 @@ from .robot_framework_activities import RobotFrameworkActivities
 
 __all__ = ["RobotFrameworkActivities"]
 
-__version__ = "1.1.1.dev0"
+__version__ = "1.1.1"
