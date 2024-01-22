0a6c699e0efa2de2a4f3c394580c2b37d91cadff
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2024-01-22 13:39:28 +0000

    Prepare next release 1.2.1.dev0

--- kuflow-robotframework/KuFlow/__init__.py
+++ kuflow-robotframework/KuFlow/__init__.py
@@ -25,7 +25,7 @@
 from .keywords import Keywords
 
 
-__version__ = "1.2.0"
+__version__ = "1.2.1.dev0"
 __all__ = ["KuFlow"]
 
 
--- kuflow-temporal-common/kuflow_temporal_common/__init__.py
+++ kuflow-temporal-common/kuflow_temporal_common/__init__.py
@@ -22,4 +22,4 @@
 # SOFTWARE.
 #
 
-__version__ = "1.2.0"
+__version__ = "1.2.1.dev0"
