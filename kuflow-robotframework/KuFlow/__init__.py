0b7a269021b030f6af24d7b2612020e709ae0a1e
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2024-09-16 12:24:44 +0100

    Prepare next release 2.0.4.dev0

--- kuflow-robotframework/KuFlow/__init__.py
+++ kuflow-robotframework/KuFlow/__init__.py
@@ -25,7 +25,7 @@
 from .keywords import Keywords
 
 
-__version__ = "2.0.3"
+__version__ = "2.0.4.dev0"
 __all__ = ["KuFlow"]
 
 
--- kuflow-temporal-common/kuflow_temporal_common/__init__.py
+++ kuflow-temporal-common/kuflow_temporal_common/__init__.py
@@ -40,4 +40,4 @@ __all__ = [
     "register_serializable_models",
 ]
 
-__version__ = "2.0.3"
+__version__ = "2.0.4.dev0"
