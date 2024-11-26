22ecd0df45a7ce4e10667772bf13c292dc96a9eb
Jorge Rodríguez Pedrianes <jrodped@kuflow.com>
2024-11-26 11:42:16 +0000

    Release 3.0.0

--- kuflow-temporal-activity-robotframework/kuflow_temporal_activity_robotframework/__init__.py
+++ kuflow-temporal-activity-robotframework/kuflow_temporal_activity_robotframework/__init__.py
@@ -27,4 +27,4 @@ from .robot_framework_activities import RobotFrameworkActivities
 
 __all__ = ["RobotFrameworkActivities"]
 
-__version__ = "2.2.0.dev0"
+__version__ = "3.0.0"
--- scripts/create_local_version.sh
+++ scripts/create_local_version.sh
@@ -6,7 +6,7 @@
 # Dev version follow the patter: X.X.X.devX
 
 ########################################
-VERSION=2.2.0.dev0
+VERSION=3.0.0
 ########################################
 
 
